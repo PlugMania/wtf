fd13939a92dcd3e2f96dadcea1ce23e7b9e201a4
nsordk <nicolai@nsor.dk>
2012-07-12 22:04:32 +0200

    More lines added

--- src/info/plugmania/wtf/wtf.java
+++ src/info/plugmania/wtf/wtf.java
@@ -5,12 +5,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class wtf extends JavaPlugin {
 
+	private wtfCommandExecutor myExecutor;
+	@Override
 	public void onEnable(){ 
 		getLogger().info("wtf is enabled.");
+		myExecutor = new wtfCommandExecutor(this);
+		getCommand("wtf").setExecutor(myExecutor);
 	}
  
 	public void onDisable(){ 
-		getLogger().info("wtf has been disabled.");
+		getLogger().info("wtf has been disabled.");	
 	}
 
 }
