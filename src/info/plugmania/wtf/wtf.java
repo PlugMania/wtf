ba0c2ea7d656acda230144d8080d4f776857ce96
nsordk <nicolai@nsor.dk>
2012-07-13 10:04:05 +0200

    Removed Commandimplementation from bukkit guide

--- src/info/plugmania/wtf/wtf.java
+++ src/info/plugmania/wtf/wtf.java
@@ -5,12 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class wtf extends JavaPlugin {
 
-	private wtfCommandExecutor myExecutor;
-	@Override
 	public void onEnable(){ 
 		getLogger().info("wtf is enabled.");
-		myExecutor = new wtfCommandExecutor(this);
-		getCommand("wtf").setExecutor(myExecutor);
 	}
  
 	public void onDisable(){ 
